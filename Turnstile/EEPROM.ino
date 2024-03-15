6bc2f62f0f1cfb5a8d2670f458af58fde7b5c705
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2024-03-15 14:56:31 +0700

    Update EEPROM.ino

--- Turnstile/EEPROM.ino
+++ Turnstile/EEPROM.ino
@@ -3,9 +3,17 @@ void simpanEEPROM (int setDelay,int setTimeout,int addressDelay,int addressTimeo
   EEPROM.write(addressDelay,setDelay);
   EEPROM.write(addressTimeout,setTimeout);
 }
-
+void simpanArah(int setArah,int addressArah)
+{
+  EEPROM.write(addressArah,setArah);
+}
 void bacaEEPROM(int addressDelay,int addressTimeout)
 {
  nilaiDelay = EEPROM.read(addressDelay);
  timeout = EEPROM.read(addressTimeout);
 }
+void bacaArah(int addressArah)
+{
+ nilaiArah = EEPROM.read(addressArah);
+}
+ 
