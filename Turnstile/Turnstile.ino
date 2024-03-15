e2c9342a58ca13b6f8ef2b8fdd71a5d7ebb75130
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2024-03-15 14:55:32 +0700

    Update Turnstile.ino

--- Turnstile/Turnstile.ino
+++ Turnstile/Turnstile.ino
@@ -7,6 +7,7 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 //====================== Pin Setup =================
 #define EEPROMDelay 0
 #define EEPROMTimeout 1
+#define EEPROMArah 3
 #define ena 7       // pin ENA
 #define dir 8       // pin Direction TB6600
 #define pul 9       // Pin pulse di TB6600
@@ -16,30 +17,28 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define pinLed1 3
 #define pinLed2 4
 #define timeCount 9
-#define pinLeft  6 
+#define pinLeft 6
 #define pinUp 11
 #define pinRight 10
 #define pinDown 7
 #define pinOk 12
 
-//================== data setup ======================
+//============================ data setup ======================================
 
 int flagIn = 0;
 String dataIn = "";
 int flagExit = 0;
-int flagMasukMenu=0;
+int flagMasukMenu = 0;
 
 int timeout = 0;
-int nilaiDelay =0;
+int nilaiDelay = 0;
+int nilaiArah = 0;
 
 int count = timeout;
 int flagTimeout = timeout;
 
-
-
-const int stepOpen = 750; //step untuk stepper motor untuk membuka gate
-const int stepClose = 750; //step untuk Stepper motor untuk menutup kembali
-
+const int stepOpen = 750;   //step untuk stepper motor untuk membuka gate
+const int stepClose = 750;  //step untuk Stepper motor untuk menutup kembali
 
 unsigned long lastDebounceTime = 0;  //variabel untuk debounce
 unsigned long debounceDelay = 50;    //waktu debounce dalam milidetik
@@ -47,14 +46,13 @@ unsigned long prevMillis = 0;        //variabel untuk menyimpan waktu sebelumnya
 unsigned long interval = 500;        //interval waktu untuk debounce sensor
 
 unsigned long previousMillisTimeOut = 0;
-unsigned long intervalTimeOut = timeout*100;
-
-
+unsigned long intervalTimeOut = timeout * 100;
 
 //============ Speed Setup NEMA ===============
 #define speedStepper 200  //step untuk buka dan tutup setting TB6600 di 400 step
 //=============================================
-void(* resetFunc) (void) = 0; 
+void (*resetFunc)(void) = 0; //to restart arduino
+
 void steadyFlip()  // fungsi untuk steady kan flip untuk posisi closed
 {
   digitalWrite(ena, LOW);  // aktifator ENA
@@ -89,12 +87,10 @@ void openSwingIn()  // fungsi untuk stepper untuk proses memnula gate
     checkTimeout();
     // Serial.println("waiting " + String (count));
     // delay(2);
-    if (digitalRead(sensor2) == LOW || flagTimeout == 1)
-    {
+    if (digitalRead(sensor2) == LOW || flagTimeout == 1) {
       digitalWrite(ena, LOW);
       digitalWrite(dir, LOW);
-      for (int i = 0; i <= stepClose; i++)
-      {
+      for (int i = 0; i <= stepClose; i++) {
         // Serial.println("step close :" + String (i));
         digitalWrite(pul, HIGH);
         delayMicroseconds(speedStepper);
@@ -125,14 +121,13 @@ void openSwingOut() {
     // Serial.print(".");
   }
   Serial.println("opened");
-  delay(5); //delay untuk holding
+  delay(5);                             //delay untuk holding
   while (digitalRead(sensor1) == HIGH)  // perulangan untuk deteksi user lewat arah exit dan menutup gate kembali
   {
     checkTimeout();
-    Serial.println("waiting " + String (count));
+    Serial.println("waiting " + String(count));
     // delay(2); //   Serial.println("Waiting");elay untuk trigeer proses
-    if (digitalRead(sensor1) == LOW || flagTimeout == 1)
-    {
+    if (digitalRead(sensor1) == LOW || flagTimeout == 1) {
       digitalWrite(ena, LOW);
       digitalWrite(dir, HIGH);
       for (int i = 0; i <= stepClose; i++)  // fungsi stepper untuk menutup turnstile
@@ -146,7 +141,7 @@ void openSwingOut() {
       }
       digitalWrite(ena, HIGH);
       Serial.println("closed");
-      delay(200); // delay diganti jadi 10ms
+      delay(200);  // delay diganti jadi 10ms
       resetAll();
       loop();
       break;
@@ -160,41 +155,34 @@ void resetAll() {
   flagExit = 0;
   flagTimeout = 0;
   count = timeout;
-  flagMasukMenu=0;
+  flagMasukMenu = 0;
 }
 
-void checkTimeout()
-{
+void checkTimeout() {
   unsigned long currentMillis = millis();
 
-  if ((unsigned long)(currentMillis - previousMillisTimeOut) >= intervalTimeOut)
-  {
+  if ((unsigned long)(currentMillis - previousMillisTimeOut) >= intervalTimeOut) {
     count--;
     Serial.println("Timeout : " + String(count));
-    if (count == 0)
-    {
+    if (count == 0) {
       flagTimeout = 1;
-    }
-    else
-    {
+    } else {
       // Serial.print(count);
     }
     previousMillisTimeOut = millis();
   }
 }
 
-
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
-  bacaEEPROM(EEPROMDelay,EEPROMTimeout);
-  Serial.println("Timeout : " + String(timeout)+ "Delay(ms) : "+ String (nilaiDelay));
+  bacaEEPROM(EEPROMDelay, EEPROMTimeout);
+  Serial.println("Timeout : " + String(timeout) + "Delay(ms) : " + String(nilaiDelay));
   pinMode(pinUp, INPUT_PULLUP);
   pinMode(pinDown, INPUT_PULLUP);
   pinMode(pinRight, INPUT_PULLUP);
   pinMode(pinLeft, INPUT_PULLUP);
   pinMode(pinOk, INPUT_PULLUP);
-  lcd.init();                      // initialize the lcd
+  lcd.init();  // initialize the lcd
   lcd.init();
   // Print a message to the LCD.
   lcd.backlight();
@@ -210,89 +198,99 @@ void setup()
   lcd.print("System Ready");
   delay(100);
   // simpanEEPROM (150,100,EEPROMDelay,EEPROMTimeout);
-  bacaEEPROM(EEPROMDelay,EEPROMTimeout);
-  Serial.println("Delay : "+ String(nilaiDelay) + " Timeout : " + String(timeout));
+  bacaArah(EEPROMArah);
+  bacaEEPROM(EEPROMDelay, EEPROMTimeout);
+  Serial.println("Delay : " + String(nilaiDelay) + " Timeout : " + String(timeout) + " Mode = " + String(nilaiArah));
   delay(10);
-  intervalTimeOut = timeout*100;
+  intervalTimeOut = timeout * 100;
   count = timeout;
   flagTimeout = timeout;
+  if(nilaiArah==3)
+  {
+    lcd.setCursor(0, 2);
+     lcd.print("System Not Activated");
+  }
 
   Serial.println(intervalTimeOut);
 }
 
 void loop() {
-  // while (1)
-  // {
-  //   digitalWrite(ena, HIGH);              //Aktifator ENA
-  //   digitalWrite(dir, LOW);             // mengatur direction untuk open set di HIGH
-  //  for (int i = 0; i <= stepOpen; i++)  //perulangan untuk per step buka (open proses)
-  //  {
-  //   // Serial.println("step open :" + String (i));
-  //   digitalWrite(pul, HIGH);
-  //   delayMicroseconds(speedStepper);
-  //   digitalWrite(pul, LOW);
-  //   delayMicroseconds(speedStepper);
-  //   // delay(10);
-  // }
-    //  bacaEEPROM(EEPROMDelay,EEPROMDelay);
-    //  Serial.print(nilaiDelay);
-    //  Serial.print(" / ");
-    //  Serial.print(timeout);
-    //  Serial.println();
-    //  menu();
-  //   Serial.println(digitalRead(pinLeft));
-  // }
-
-  digitalWrite(pinLed1, HIGH);
-  digitalWrite(pinLed2, LOW);
+  if (nilaiArah==3)
+  {
+    digitalWrite(pinLed1, HIGH);
+    digitalWrite(pinLed2, LOW);
+  }
+  if (nilaiArah !=3)
+  {
+    digitalWrite(pinLed1, LOW);
+    digitalWrite(pinLed2, HIGH);
+  }
   unsigned long currentMillis = millis();        //mendapatkan waktu saat ini
   if (currentMillis - prevMillis >= interval) {  //jika interval waktu sudah terlewati
     prevMillis = currentMillis;                  //memperbarui waktu sebelumnya
 
     if (digitalRead(sensor1) == LOW)  // deteksi untuk arah masuk
     {
-      Serial.println("in");
-      flagIn = 1;
-      while (flagIn == 1)
-      {
-        if (Serial.available() > 0) {
-          delay(10);
-          dataIn = Serial.readString();
-          if (dataIn == "o" || dataIn == "O" || dataIn == "o\n" || dataIn == "O\n")
-          {
-            digitalWrite(pinLed1, LOW);
-            digitalWrite(pinLed2, HIGH);
-            // Serial.println("masuk");
-            openSwingIn();  // proses open arah Enter
+      if (nilaiArah == 0 || nilaiArah == 1) {
+        Serial.println("in");
+        flagIn = 1;
+        while (flagIn == 1) {
+          if (Serial.available() > 0) {
+            delay(10);
+            dataIn = Serial.readString();
+            if (dataIn == "o" || dataIn == "O" || dataIn == "o\n" || dataIn == "O\n") {
+              digitalWrite(pinLed1, HIGH);
+              digitalWrite(pinLed2, LOW);
+              // Serial.println("masuk");
+              openSwingIn();  // proses open arah Enter
+            }
           }
         }
-      }
-    }
-    else if (digitalRead(sensor2) == LOW)  //deteksi user untuk arah keluar
-    {
-      Serial.println("exit");
-
-      flagExit = 1;
-      while (flagExit == 1) {
-        if (Serial.available() > 0)
-        {
-          delay(10);
-          dataIn = Serial.readString();
-          if (dataIn == "o" || dataIn == "O" || dataIn == "o\n" || dataIn == "O\n")
-          {
-            digitalWrite(pinLed1, LOW);
-            digitalWrite(pinLed2, HIGH);
-            openSwingOut();  // proses open arah exit
-          }
+      } else {
+        Serial.println("Salah Arah Masuk");
+        for (int x = 0; x <= 10; x++) {
+          
+          digitalWrite(pinLed1, HIGH);
+          digitalWrite(pinLed2, LOW);
+          delay(100);
+          digitalWrite(pinLed1, LOW);
+          digitalWrite(pinLed2, LOW);
+          delay(100);
         }
       }
-    }
-    else if(digitalRead(pinOk)==LOW && digitalRead(pinLeft)==LOW)
+    } else if (digitalRead(sensor2) == LOW)  //deteksi user untuk arah keluar
     {
+      if (nilaiArah == 0 || nilaiArah == 2) {
+        Serial.println("exit");
+        flagExit = 1;
+        while (flagExit == 1) {
+          if (Serial.available() > 0) {
+            delay(10);
+            dataIn = Serial.readString();
+            if (dataIn == "o" || dataIn == "O" || dataIn == "o\n" || dataIn == "O\n") {
+              digitalWrite(pinLed1, HIGH);
+              digitalWrite(pinLed2, LOW);
+              openSwingOut();  // proses open arah exit
+            }
+          }
+        }
+      } else {
+        Serial.println("Salah Arah keluar");
+        for (int x = 0; x <= 5; x++) {
+          
+          digitalWrite(pinLed1, HIGH);
+          digitalWrite(pinLed2, LOW);
+          delay(100);
+          digitalWrite(pinLed1, LOW);
+          digitalWrite(pinLed2, LOW);
+          delay(100);
+          
+        }
+      }
+    } else if (digitalRead(pinOk) == LOW && digitalRead(pinLeft) == LOW) {
       // flagMasukMenu=1;
       Serial.print("masuk");
-      while(1)
-      {
+      while (1) {
         menu();
       }
     }
